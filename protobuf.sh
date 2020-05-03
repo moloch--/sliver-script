5c623d5abf730fd76eca58f20eccce1fa0d5a2b6
moloch-- <875022+moloch--@users.noreply.github.com>
2020-05-03 13:06:39 -0500

    Setting up npm package

---
+++ protobuf.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#   Sliver Implant Framework
+#   Copyright (C) 2019  Bishop Fox
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+# Path to this plugin
+PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
+PROTOC_GEN_GRPC_PATH="./node_modules/.bin/grpc_tools_node_protoc_plugin"
+
+OUT_DIR="./src/pb"
+
+# Directory to write generated code to (.js and .d.ts files)
+mkdir -p ./src/pb
+
+protoc \
+    -I sliver/protobuf \
+    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --ts_out="${OUT_DIR}" \
+    sliver/protobuf/commonpb/common.proto
+
+protoc \
+    -I sliver/protobuf \
+    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --ts_out="${OUT_DIR}" \
+    sliver/protobuf/sliverpb/sliver.proto
+
+protoc \
+    -I sliver/protobuf \
+    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --ts_out="${OUT_DIR}" \
+    sliver/protobuf/clientpb/client.proto
+
+protoc \
+    -I sliver/protobuf \
+    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH} \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --ts_out="service=grpc-node:${OUT_DIR}" \
+    --grpc_out="${OUT_DIR}" \
+    sliver/protobuf/rpcpb/services.proto
