8bb1759a9763f0aaab25634deae35723e389211d
moloch-- <875022+moloch--@users.noreply.github.com>
2020-11-30 22:05:38 -0600

    Put events enum into seperate pkg

---
+++ src/events/index.ts
@@ -0,0 +1,12 @@
+export enum Events {
+    ServerError = 'server-error',
+    SessionConnected = 'session-connected',
+    SessionDisconnected = 'session-disconnected',
+    ClientJoined = 'client-joined',
+    ClientLeft = 'client-left',
+    Canary = 'canary',
+    JobStarted = 'job-started',
+    JobStopped = 'job-stopped',
+    BuildCompleted = 'build-completed',
+    Profile = 'profile'
+};
---
+++ src/tests/events.test.ts
@@ -0,0 +1,6 @@
+
+import { Events } from '../events';
+
+test('Import Events', () => {
+    expect(Events.Canary).toBe(Events.Canary);
+});
