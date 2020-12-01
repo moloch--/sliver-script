8296a7c68bc68ba9470e306d813f4939afcb10b5
moloch-- <875022+moloch--@users.noreply.github.com>
2020-11-30 21:44:35 -0600

    Added event constants

--- src/client.ts
+++ src/client.ts
@@ -41,6 +41,20 @@ export interface Tunnel {
 }
 
 
+export enum Events {
+  ServerError = 'server-error',
+  SessionConnected = 'session-connected',
+  SessionDisconnected = 'session-disconnected',
+  ClientJoined = 'client-joined',
+  ClientLeft = 'client-left',
+  Canary = 'canary',
+  JobStarted = 'job-started',
+  JobStopped = 'job-stopped',
+  BuildCompleted = 'build-completed',
+  Profile = 'profile'
+};
+
+
 export class InteractiveSession {
 
   private _rpc: rpcpb.SliverRPCClient;
