ee6bc43e642daf86f1eadd16aa2ebd3c01d98883
moloch-- <875022+moloch--@users.noreply.github.com>
2022-02-13 12:23:34 -0600

    addes sessions test

---
+++ src/tests/sessions.test.ts
@@ -0,0 +1,27 @@
+import * as os from 'os'
+import * as path from 'path'
+
+import { ParseConfigFile } from '../config'
+import { SliverClient } from '../client'
+
+
+const DEFAULT_CONFIG_PATH = path.join(os.homedir(), '.sliver-client', 'configs', 'default.cfg')
+const SLIVER_CONFIG_FILE = process.env['SLIVER_CONFIG_FILE'] || DEFAULT_CONFIG_PATH;
+
+test('autenticate to server', async () => {
+    console.log(`Loading config: ${SLIVER_CONFIG_FILE}`)
+    const config = await ParseConfigFile(SLIVER_CONFIG_FILE)
+    const client = new SliverClient(config)
+
+    console.log(`Connecting to ${config.lhost} ...`)
+    try {
+        await client.connect()
+
+        let sessions = await client.sessions()
+        console.log(sessions)
+
+        await client.disconnect()
+    } catch (err) {
+        console.error(err)
+    }
+})
